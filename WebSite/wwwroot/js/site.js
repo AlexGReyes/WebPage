e1b39cd1ef935bfe556228a0799cbb00d1bc6100
Alejandro reyes <agreyesaldeco@gmail.com>
2021-11-24 11:51:37 -0800

    added website

--- WebSite/wwwroot/js/site.js
+++ WebSite/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = -560;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    var total = (window.innerWidth / 2) - (560 / 2);
+    function frame() {
+        if (pos == total) {
+            clearInterval(id);
+        } else {
+            pos = pos + 2;
+            elem.style.left = pos + "px";
+        }
+    }
+}
